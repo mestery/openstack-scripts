9a5368926583f844b4145cda37716cf353749d3e
Kyle Mestery <mestery@mestery.com>
2016-02-02 16:29:08 -0600

    Create our own flavor
    Signed-off-by: Kyle Mestery <mestery@mestery.com>

--- ovn/quota.sh
+++ ovn/quota.sh
@@ -6,5 +6,6 @@ neutron quota-update --tenant_id $PROJECTID --network 10000
 neutron quota-update --tenant_id $PROJECTID --router 5000
 neutron quota-update --tenant_id $PROJECTID --port 20000
 neutron quota-update --tenant_id $PROJECTID --subnet 10000
+openstack flavor create --ram 64 --disk 0 --vcpus 1 --id 27 ovntenant
 nova quota-update --instances 50000 ovntenant
 nova quota-show --tenant ovntenant
