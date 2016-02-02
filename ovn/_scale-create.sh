9340ffe16d9c84f9da2d6ceb76059eccfae9539a
Kyle Mestery <mestery@mestery.com>
2016-02-02 09:26:14 -0600

    Ensure subnets are non-overlapping
    Signed-off-by: Kyle Mestery <mestery@mestery.com>

--- ovn/_scale-create.sh
+++ ovn/_scale-create.sh
@@ -8,7 +8,7 @@ subm=$((($n + 1) % 256))
 neutron net-create ovnnet-a-$n
 neutron net-create ovnnet-b-$n
 neutron subnet-create --name ovnsubnet-a-$n ovnnet-a-$n 10.$subn.$subm.0/24
-neutron subnet-create --name ovnsubnet-b-$n ovnnet-b-$n 10.$subn.$subm.0/24
+neutron subnet-create --name ovnsubnet-b-$n ovnnet-b-$n 10.$subm.$subn.0/24
 neutron router-create ovnrouter-$n
 neutron router-interface-add ovnrouter-$n subnet=ovnsubnet-a-$n
 neutron router-interface-add ovnrouter-$n subnet=ovnsubnet-b-$n
