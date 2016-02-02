bb1a9f0955b166abb44d274da99d2e59f46d36fc
Kyle Mestery <mestery@mestery.com>
2016-02-02 09:22:29 -0600

    Create differing CIDRs for subnets on the same router
    Ensure we create non-overlapping subnets here.
    
    Signed-off-by: Kyle Mestery <mestery@mestery.com>

--- ovn/_scale-create.sh
+++ ovn/_scale-create.sh
@@ -2,11 +2,13 @@
 
 # NOTE: The 5 below needs to match PER_RUN in scale.sh
 n=`echo $1 \* 5 + $2 | bc`
+subn=$(($n % 256))
+subm=$((($n + 1) % 256))
 #echo "Create N is $n"
 neutron net-create ovnnet-a-$n
 neutron net-create ovnnet-b-$n
-neutron subnet-create --name ovnsubnet-a-$n ovnnet-a-$n 10.$1.$2.0/24
-neutron subnet-create --name ovnsubnet-b-$n ovnnet-b-$n 10.$2.$1.0/24
+neutron subnet-create --name ovnsubnet-a-$n ovnnet-a-$n 10.$subn.$subm.0/24
+neutron subnet-create --name ovnsubnet-b-$n ovnnet-b-$n 10.$subn.$subm.0/24
 neutron router-create ovnrouter-$n
 neutron router-interface-add ovnrouter-$n subnet=ovnsubnet-a-$n
 neutron router-interface-add ovnrouter-$n subnet=ovnsubnet-b-$n
