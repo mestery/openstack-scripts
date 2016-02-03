69e61a0ee764d45b2eba330ecd148dd0f77c73ed
Kyle Mestery <mestery@mestery.com>
2016-02-03 09:14:52 -0600

    Add a SG rule to allow pings to the default SG
    Signed-off-by: Kyle Mestery <mestery@mestery.com>

--- ovn/quota.sh
+++ ovn/quota.sh
@@ -8,6 +8,9 @@ neutron quota-update --tenant_id $PROJECTID --port 20000
 neutron quota-update --tenant_id $PROJECTID --subnet 10000
 openstack flavor create --ram 64 --disk 0 --vcpus 1 --id 27 ovntenant
 
+# Add a SG rule to allow ping
+neutron security-group-rule-create --direction ingress --protocol icmp default
+
 OVN_TENANT=$(openstack project show -f value -c id ovntenant)
 nova quota-update --instances 50000 --cores 1000 --ram 1024000 $OVN_TENANT
 nova quota-show --tenant $OVN_TENANT
