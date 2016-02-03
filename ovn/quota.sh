583e3a6df2acca116c555b68a52fce43aacb57b3
Kyle Mestery <mestery@mestery.com>
2016-02-03 08:52:23 -0600

    Correctly set nova quotas for the ovntenant
    Signed-off-by: Kyle Mestery <mestery@mestery.com>

--- ovn/quota.sh
+++ ovn/quota.sh
@@ -7,5 +7,7 @@ neutron quota-update --tenant_id $PROJECTID --router 5000
 neutron quota-update --tenant_id $PROJECTID --port 20000
 neutron quota-update --tenant_id $PROJECTID --subnet 10000
 openstack flavor create --ram 64 --disk 0 --vcpus 1 --id 27 ovntenant
-nova quota-update --instances 50000 --cores 1000 --ram 1024000 ovntenant
-nova quota-show --tenant ovntenant
+
+OVN_TENANT=$(openstack project show -f value -c id ovntenant)
+nova quota-update --instances 50000 --cores 1000 --ram 1024000 $OVN_TENANT
+nova quota-show --tenant $OVN_TENANT
