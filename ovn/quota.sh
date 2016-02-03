b2584a2661a29b06491c14bfd12b89f4f08880be
Kyle Mestery <mestery@mestery.com>
2016-02-03 08:36:53 -0600

    Update cores and ram quota for the ovntenant user
    Signed-off-by: Kyle Mestery <mestery@mestery.com>

--- ovn/quota.sh
+++ ovn/quota.sh
@@ -7,5 +7,5 @@ neutron quota-update --tenant_id $PROJECTID --router 5000
 neutron quota-update --tenant_id $PROJECTID --port 20000
 neutron quota-update --tenant_id $PROJECTID --subnet 10000
 openstack flavor create --ram 64 --disk 0 --vcpus 1 --id 27 ovntenant
-nova quota-update --instances 50000 ovntenant
+nova quota-update --instances 50000 --cores 1000 --ram 1024000 ovntenant
 nova quota-show --tenant ovntenant
